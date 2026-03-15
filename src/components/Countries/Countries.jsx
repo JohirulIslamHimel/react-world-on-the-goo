78250f9e470e947ea3798014c938d03559ab558c
Johirul Islam Himel <himel1199@gmail.com>
2026-03-16 01:29:03 +0600

    Lift up state in arrays by comparing reference

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,36 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
+// import { Linter } from "eslint";
 
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = (country) => {
+    console.log("Visited country clicked", country);
+    // visitedCountries.push(country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   //   console.log(countries);
   return (
     <div>
       <h1>In the countries : {countries.length}</h1>
+      <h3>Total Country Visited : {visitedCountries.length} </h3>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li key={country.cca3.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.cca3.cca3} country={country}></Country>
+          <Country
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   //   console.log(country.name.common);
   const [visited, setVisited] = useState(false);
+  console.log("Country visited");
 
   const handleVisited = () => {
     // console.log("Button Clicked");
@@ -18,6 +19,7 @@ const Country = ({ country }) => {
     // setVisited(visited ? false : true);
     // 3rd system
     setVisited(!visited);
+    handleVisitedCountries(country);
   };
 
   return (
